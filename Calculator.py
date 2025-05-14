f7ef6653b5fa469df66c0638cac960b441448cd4
Shankar0211 <shankarnainala05605@gmail.com>
2025-05-14 17:36:37 +0530

    Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,26 @@
+#simple AL calculations program
+#doing addition between two numbers
 def Plusal(a, b):
     return a + b
+#doing subtraction between two numbers
 def Minal(a, b):
     return a - b
+#doing multiplication between two numbers
 def Multiple(a, b):
     return a * b
+#doing division between two numbers
 def Dividal(a, b):
     return a / b
+#to print the operation names
 print("Select Arithmetic operation:\n")
 print("1.Plusal","(add)")
 print("2.Minal","(subtract)")
 print("3.Multiple","(multiply)")
 print("4.Dividal","(divide)")
 while True:
+#to take the input from the user
     choice = input("Enter the choice(1/2/3/4):")
+#to choose one of the four operations
     if choice in('1','2','3','4'):
         try:
             num1 = float(input("Enter first value:"))
@@ -31,6 +39,8 @@ while True:
                 print("Error: Cannot divide by zero.")
             else:    
                 print(num1,"/", num2,  "=", Dividal(num1, num2))
+#to take the next calculation if the user wants
+#break the loop if the user's answer in "cancel"
         next_calculation = input("Let's do next calculation? (ok/cancel):")
         if next_calculation == "cancel":
             break
